6646924f2bbba09976114bf1a5edb93c5deec88a
Dolmar Velasquez <61443234+dodoVelasquez@users.noreply.github.com>
2025-05-22 20:07:29 -0600

    Update Querys.sql

--- Serie1/Querys.sql
+++ Serie1/Querys.sql
@@ -1,1 +1,48 @@
-Querys
+CREATE TABLE TblActivo (
+    ActivoID INT PRIMARY KEY IDENTITY,
+    Nombre VARCHAR(100),
+    Descripcion VARCHAR(255),
+    Tipo VARCHAR(50), -- 'Equipo' o 'Mobiliario'
+    Valor DECIMAL(18, 2),
+    Estado VARCHAR(50), -- Vigente, Obsoleto, Baja
+    FechaAdquisicion DATETIME,
+    CodigoInventario VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE TblEstado (
+    EstadoID INT PRIMARY KEY IDENTITY,
+    Descripcion VARCHAR(20),
+);
+
+CREATE TABLE TblDepartamento (
+    DepartamentoID INT PRIMARY KEY IDENTITY,
+    Nombre VARCHAR(100)
+);
+
+CREATE TABLE TblUsuario (
+    UsuarioID INT PRIMARY KEY IDENTITY,
+    Nombre VARCHAR(100),
+    DepartamentoID INT FOREIGN KEY REFERENCES TblDepartamento(DepartamentoID)
+);
+
+CREATE TABLE TblUbicacion (
+    UbicacionID INT PRIMARY KEY IDENTITY,
+    Descripcion NVARCHAR(100)
+);
+
+
+CREATE TABLE TblMovimientos (
+    MovimientoID INT PRIMARY KEY IDENTITY,
+    ActivoID INT,
+    FechaMovimiento DATETIME DEFAULT GETDATE(),
+    UsuarioID INT NULL,
+    DepartamentoID INT NULL,
+    UbicacionID INT NULL,
+    Observaciones NVARCHAR(255)
+);
+
+
+CREATE TABLE TblTipoMovimiento (
+    TipoMovimientoID INT PRIMARY KEY IDENTITY,
+    Descripcion VARCHAR(20),
+);
